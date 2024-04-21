25c2b156a38aec89456fde08038b4ea10e4ba4aa
lyndajungcoderacademy <15231@coderacademy.edu.au>
2024-04-21 14:30:04 +1000

    new secret number game

--- conditionals.py
+++ conditionals.py
@@ -23,9 +23,21 @@ elif user_input == 6:
 else:
     print("out of range")
 
-secret_number = 3
-try = input('Guess the number between 1-10:> ')
+#guess secret number
+
+import random
+
+secret_number = random.randint(0,9)
+guess = input('Guess the number between 1-10:> ')
+if guess.isdigit():
+   guess = int(guess)
+   if guess == secret_number:
+       print('You guessed the correct number! You win!')
+   elif guess > secret_number and guess <= 9:
+       print('You guessed too high. Sorry you lose!')
+   elif guess < secret_number and guess >= 0:
+       print('You guessed too low. Sorry you lose!')
    else:
        print('Out of range')
 else:
-    print('That\'s not even an integer!')
+   print('That\'s not even an integer! What are you playing at?!')
